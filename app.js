8735603d154703ae2b85e21b5c385b7847985611
Meshack Korir <meshackkorir100@gmail.com>
2023-10-22 22:22:28 +0300

    displaed none

---
+++ app.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function(){
+    // Getting elements
+
+    const sideTwo = document.querySelector(".side-two");
+    const numbers = document.querySelectorAll(".circle");
+    const clickedNumberPage = document.getElementById("clicked-number-page");
+    const clickedNumber = document.getElementById("clicked-number");
+
+    // Hiding the "side-two" class page
+    sideTwo.style.display = "none";
+
+})
---
+++ example.js
@@ -0,0 +1,14 @@
+<div class="hide success">
+    <div class="contents">
+        <div class="icon">
+            <img src="./assets/images/icon-list.svg" alt="icon" />
+        </div>
+        <h1>Thanks for subscribing!</h1>
+        <p>
+            A confirmation email has been sent to
+            <span id="email_addres">example@email.com</span>.Please open it and
+            click the button inside to confirm you Subscription
+        </p>
+    </div>
+    <button id="dismiss_btn" class="button">Dismiss message</button>
+</div>
